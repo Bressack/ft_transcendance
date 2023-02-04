a5239c15adca08bd79470b2fc1e5e4c6724e35c8
Aymeric SINGEVIN <ssingevi@e1r5p7.clusters.42paris.fr>
2023-02-04 17:27:09 +0100

    the spectacle is ok 2

--- src/pages/Game/Spectate.vue
+++ src/pages/Game/Spectate.vue
@@ -14,30 +14,8 @@ import DrawGame3d from './components/DrawGame3d.vue'
 
 export default defineComponent({
 	name: 'Spectate',
-	data()
-	{
-		return {
-			div2d : <HTMLElement>document.getElementById('2d'),
-			div3d : <HTMLElement>document.getElementById('3d'),
-			viewmod: false,
-			viewside:true,
-		}
-	},
 	components: {DrawGame,DrawGame3d},
-	methods :
-	{
-		changeViewSide()
-		{
-			console.log(this.viewside);
-			if (this.viewside)
-				this.viewside = false;
-			else
-				this.viewside = true;
-		}
-	},
 	mounted() {
-		this.div2d = <HTMLElement>document.getElementById('2d')
-		this.div3d = <HTMLElement>document.getElementById('3d')
 		this.$ws.socket.emit("watch-game", this.$route.params.gameId, (response : string) => {
 			if (response === "NOT_FOUND")
 				return
