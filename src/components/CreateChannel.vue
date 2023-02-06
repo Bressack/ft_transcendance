db5e8bd3df2be4ace34c69dfabd6f9e1426bff7d
adebureaux <adebureaux@gmail.com>
2023-02-06 18:39:02 +0100

    create channel rework

--- src/components/CreateChannel.vue
+++ src/components/CreateChannel.vue
@@ -31,46 +31,13 @@
 
 
     <q-item class="label">
-      <!-- <q-checkbox v-model="public" label="public" /> -->
-      <q-checkbox v-model="private" color="white" label="Private" />
+      <q-checkbox v-model="access" color="white" label="Private channel" />
     </q-item>
 
 
-
-<q-form v-if="private" class="q-pa-sm" style="max-width: 300px" @submit="search()">
-
-  <q-item>
-    <q-item-section>
-      <q-input v-model="searchInput" color="orange" label="Search User by name" @update:model-value="search()" />
-    </q-item-section>
-    <q-item-section style="max-width: 30px" v-if="searchInput.length" @click="clearInput">
-      <q-btn dense flat color="red" icon="cancel" @click="search()" />
-    </q-item-section>
-    <q-item-section style="max-width: 10px">
-      <q-btn dense flat color="orange" icon="search" @click="search()" />
-    </q-item-section>
-  </q-item>
-
-  <q-list class="q-py-md overall" v-if="searchResult?.result?.length">
-    <q-item v-ripple v-for="item in searchResult?.result" :key="item.username">
-      <q-item-section style="max-width: 50px;">
-        <q-avatar class="avatar">
-          <img size="20px" :src="`/api/avatar/${item?.username}/thumbnail`">
-        </q-avatar>
-      </q-item-section>
-      <q-item-section>
-        <q-item-label>
-          {{ item?.username }}
-        </q-item-label>
-      </q-item-section>
-      <q-item-section side>
-        <q-icon name="add" color="green" @click="add(item?.username)"/>
-      </q-item-section>
+    <q-item v-if="access">
+        Search
     </q-item>
-  </q-list>
-</q-form>
-
-
 
   <q-item class="flex-center q-pb-md">
     <q-btn color="orange" label="Create" @click="create()"/>
@@ -80,63 +47,26 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import { ISearchQuery } from 'src/services/api.models'
 
-interface IResult {
-	total: number,
-	result: Array<IUserName>
-}
-
-interface IUserName {
-	username: String
-}
+// POST /users/channel
 
 export default defineComponent({
 	name: 'CreateChannel',
-  // setup () {
-  //   return {
-  //     access: ref('public'),
-  //     options: [
-  //       {
-  //         label: 'PUBLIC',
-  //         value: 'public'
-  //       },
-  //       {
-  //         label: 'PRIVATE',
-  //         value: 'private'
-  //       }
-  //     ]
-  //   }
-  // },
+  setup () {
+    return {
+      access : ref(false)
+    }
+  },
   data() {
     return {
       name: '' as string,
       password: '' as string,
-      private: false as boolean,
-      searchInput: '',
-			searchResult: {} as IResult,
-      userList: [] as any
+      usernames: [] as string[]
     }
   },
   methods: {
-		search() {
-			if (this.searchInput.length == 0) {
-				this.searchResult = {} as IResult
-				return;
-			}
-			let that = this
-			const searchQuery: ISearchQuery = { key: this.searchInput }
-			this.$api.search(searchQuery)
-				.then(function (result) {
-					that.searchResult = result
-				})
-				.catch(function (error) { })
-		},
-    add (username : string) {
-      this.userList.push(username)
-    },
-    create () {
-      console.log(this.userList)
+    create() {
+      console.log(this.usernames)
     }
   }
 })
