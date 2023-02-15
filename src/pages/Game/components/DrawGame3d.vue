4ffd06f9362623e45d71c602a8b38175835603a6
Aymeric SINGEVIN <ssingevi@e1r12p2.clusters.42paris.fr>
2023-02-15 14:04:08 +0100

    start new feature

--- src/pages/Game/components/DrawGame3d.vue
+++ src/pages/Game/components/DrawGame3d.vue
@@ -12,36 +12,45 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, computed } from 'vue'
+import {defineComponent} from 'vue'
 import { watch } from 'vue'
-import { throttle } from 'lodash'
 import * as THREE from 'three';
 import { useMeStore } from '../../../stores/me';
 // import THREE from 'three';
 
+var	paddle1Material = new THREE.MeshBasicMaterial({color: 0x00ffff});
+var	paddle2Material = new THREE.MeshBasicMaterial({color: 0xFF0000});
+var	planeMaterial = new THREE.MeshBasicMaterial({color: 0x242729});
+// var planeMaterial = new THREE.MeshBasicMaterial({ map: loader.load( 'https://cdn.intra.42.fr/users/d0f4dd0d898a9e9cdb2df466d0e5c944/aribesni.jpg' )});
+// var planeMaterial = new THREE.MeshBasicMaterial({ map: loader.load( 'https://upload.wikimedia.org/wikipedia/commons/3/38/Xavier_Niel004.jpg' )});
+var	tableMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF}); 
+var	sphereMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
+var	loader = new THREE.TextureLoader();
 
-var timeOutFunctionId = undefined as any;
+var		timeOutFunctionId = undefined as any;
 // scene object variables
-var renderer = null as any
-var scene = null as any
-var	camera = null as any
-// var	pointLight = null as any
-var spotLight= null as any
+var		scene = new THREE.Scene();
+var		renderer = new THREE.WebGLRenderer();
+var		camera = null as any
 
-// field variables
-var fieldWidth = 1150, fieldHeight = 725;
+// field const
+const	fieldWidth = 1150, fieldHeight = 725;
 
-// paddle variables
-var paddleWidth = 10;
-var paddleHeight = 90;
-var paddleDepth = 10;
-// var paddleQuality = 10
-// var paddle1DirY = 0, paddle2DirY = 0, paddleSpeed = 3;
+// paddle const
+const	paddleWidth = 10, paddleHeight = 90, paddleDepth = 10;
 
-// ball variables
-var ball = null as any
-var paddle1= null as any
-var paddle2 = null as any
+// ball const
+const	radius = 10, segments = 6, rings = 6;
+
+// camera const
+const	VIEW_ANGLE = 50, NEAR = 0.1, FAR = 5000;
+
+// geometric elements
+var ball =  new THREE.Mesh(new THREE.SphereGeometry(radius, segments, rings), sphereMaterial);
+var paddle1=  new THREE.Mesh(new THREE.BoxGeometry(paddleWidth, paddleHeight, paddleDepth), paddle1Material);
+var paddle2 =  new THREE.Mesh(new THREE.BoxGeometry(paddleWidth, paddleHeight, paddleDepth,),paddle2Material);
+var plane =  new THREE.Mesh(new THREE.PlaneGeometry(fieldWidth, fieldHeight ), planeMaterial);
+var table =  new THREE.Mesh(new THREE.PlaneGeometry(fieldWidth * 1.03, fieldHeight * 1.03), tableMaterial);
  
 
 export default defineComponent({
@@ -53,20 +62,13 @@ export default defineComponent({
 			canvas: <HTMLCanvasElement> document.getElementById('gameCanvas'),
 			canvas_txt : <HTMLCanvasElement>document.getElementById('canvas'),
 			gameId: "",
-			throttleValue: 15,
-			player_height: 90,
-			player_width: 5,
-			ball_x: 550,
-			ball_y: 360,
-			ball_r: 5,
-			player1_y: 310,
-			player2_y: 310,
 			player1_score: 0,
 			player2_score: 0,
 			storeMe: useMeStore(),
 			playerOneName: "p1",
 			playerTwoName: "p2",
 			prevviewside: false,
+			context: null as any
 		}
 	},
 	props: 
@@ -84,75 +86,30 @@ export default defineComponent({
 		{
 			let HEIGHT = window.innerWidth / 1.5 / 1.6;
 			let WIDTH = window.innerWidth / 1.5;
-			var VIEW_ANGLE = 50, ASPECT = WIDTH / HEIGHT, NEAR = 0.1, FAR = 5000;
-			renderer = new THREE.WebGLRenderer();
-			
-			camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-			scene = new THREE.Scene();
-			scene.add(camera);
-			camera.position.z = 320;
+			camera = new THREE.PerspectiveCamera( VIEW_ANGLE,  WIDTH / HEIGHT, NEAR, FAR);
+			scene.background = loader.load( 'https://cdn.sortiraparis.com/images/1001/94880/721017-espace-un-objet-mysterieux.jpg' );
 			renderer.setSize(WIDTH, HEIGHT);
 			renderer.domElement.id = 'testid';
-			const loader = new THREE.TextureLoader();
-			// scene.background = 
-			scene.background = loader.load( 'https://cdn.sortiraparis.com/images/1001/94880/721017-espace-un-objet-mysterieux.jpg' );
-			this.canvas.appendChild(renderer.domElement);
-			var planeWidth = fieldWidth
-			var	planeHeight = fieldHeight		
-			var paddle1Material = new THREE.MeshBasicMaterial({color: 0x00ffff});
-			var paddle2Material = new THREE.MeshBasicMaterial({color: 0xFF0000});
-
-			var planeMaterial = new THREE.MeshBasicMaterial({ map: loader.load( 'https://upload.wikimedia.org/wikipedia/commons/3/38/Xavier_Niel004.jpg' )});
-			
-			var tableMaterial = new THREE.MeshBasicMaterial({color: 0x8c8c8c});
-			var tableMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
-			var sphereMaterial = new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
-			var plane = new THREE.Mesh( new THREE.PlaneGeometry(planeWidth, planeHeight ), planeMaterial);
+			scene.add(camera);
 			scene.add(plane);
-			var table = new THREE.Mesh(
-			new THREE.PlaneGeometry( planeWidth * 1.03, planeHeight * 1.03), tableMaterial);
-			table.position.z = -5;
 			scene.add(table);
-			var radius = 10;
-			var	segments = 6;
-			var	rings = 6;
-			ball = new THREE.Mesh(new THREE.SphereGeometry( radius, segments, rings), sphereMaterial);
 			scene.add(ball);
-
+			scene.add(paddle1);
+			scene.add(paddle2);
+			this.canvas.appendChild(renderer.domElement);	
+			/*
+				setup elements pos
+			*/ 
+			camera.position.z = 320;
+			table.position.z = -5;
 			ball.position.x = 0;
 			ball.position.y = 0;
-			ball.position.z = radius;	
-			paddle1 = new THREE.Mesh(
-			  new THREE.BoxGeometry(
-				paddleWidth,
-				paddleHeight,
-				paddleDepth,
-			  )
-				,
-			  paddle1Material);
-			scene.add(paddle1);
-			paddle1.receiveShadow = true;
-    		paddle1.castShadow = true;
-			paddle2 = new THREE.Mesh(
-			  new THREE.BoxGeometry(
-				paddleWidth,
-				paddleHeight,
-				paddleDepth,
-			  )
-				,
-			  paddle2Material);
-			scene.add(paddle2);
-			paddle2.receiveShadow = true;
-    		paddle2.castShadow = true;	
+			ball.position.z = radius;
 			paddle1.position.x = -fieldWidth/2 + paddleWidth + 15;
 			paddle2.position.x = fieldWidth/2 - paddleWidth - 15;
 			paddle1.position.z = paddleDepth;
 			paddle2.position.z = paddleDepth;
-    		spotLight = new THREE.SpotLight(0xF8D898);
-    		spotLight.position.set(0, 0, 460);
-    		spotLight.intensity = 1.5;
-    		spotLight.castShadow = true;
-    		scene.add(spotLight);
+			camera.position.z = paddleDepth + 800;
 			this.cameraPhysics()	
 		},
 		draw()
@@ -160,37 +117,35 @@ export default defineComponent({
 			// paddle1.position.y = 310
 			this.cameraPhysics();
 			const elementsColor: string = this.game_paused && this.info_value ? "#242729" : "white";
-
-			// console.log(this.canvas)
-        	var context = <CanvasRenderingContext2D>this.canvas_txt.getContext("2d");
-				context.clearRect(0,0,this.canvas_txt.width,this.canvas_txt.height)
+        	this.context = <CanvasRenderingContext2D>this.canvas_txt.getContext("2d");
+				this.context.clearRect(0,0,this.canvas_txt.width,this.canvas_txt.height)
 			if (this.game_paused == true)
 			{
-				context.globalAlpha = 0.5;
-				context.fillStyle = "#242729";
-				context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+				this.context.globalAlpha = 0.5;
+				this.context.fillStyle = "#242729";
+				this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 			}
 			{
-				context.globalAlpha = 1;
+				this.context.globalAlpha = 1;
 			}
-        	context.fillStyle = elementsColor;
-        	context.fill();
-        	context.font = `${this.canvas_txt.height * 0.07}px 'Press Start 2P'`;
-        	context.fillText(
+        	this.context.fillStyle = elementsColor;
+        	this.context.fill();
+        	this.context.font = `${this.canvas_txt.height * 0.07}px 'Press Start 2P'`;
+        	this.context.fillText(
         	    this.player1_score.toString(),
         	    this.canvas_txt.width / 2 -
-        	        (this.canvas_txt.width * 0.05 + context.measureText(this.player1_score.toString()).width),
+        	        (this.canvas_txt.width * 0.05 + this.context.measureText(this.player1_score.toString()).width),
         	    this.canvas_txt.height * 0.1
         	);
-        	context.fillText(
+        	this.context.fillText(
         	    this.player2_score.toString(),
         	    this.canvas_txt.width / 2 + this.canvas_txt.width * 0.05,
         	    this.canvas_txt.height * 0.1
         	);
         	if (this.game_paused && this.info_value) {
-        	    const textSize = context.measureText(this.info_value);
-        	    context.fillStyle = "white";
-        	    context.fillText(this.info_value, this.canvas_txt.width / 2 - textSize.width / 2, this.canvas_txt.height / 2);
+        	    const textSize = this.context.measureText(this.info_value);
+        	    this.context.fillStyle = "white";
+        	    this.context.fillText(this.info_value, this.canvas_txt.width / 2 - textSize.width / 2, this.canvas_txt.height / 2);
         	}
 			renderer.render(scene, camera);
 		},
@@ -203,8 +158,8 @@ export default defineComponent({
 				// p1 position
 				camera.position.y += (paddle1.position.y - camera.position.y) * 0.001;
 				camera.position.x = paddle1.position.x - 300;
-				camera.position.z = paddle1.position.z + 800;
 				camera.rotation.x = 0;
+				// camera.position.z = paddleDepth + 800;
 				camera.rotation.y = -40 * Math.PI/180;
 				camera.rotation.z = -90 * Math.PI/180;
 			}
@@ -213,8 +168,8 @@ export default defineComponent({
 				// p2 position
 				camera.position.y += (paddle2.position.y - camera.position.y) * 0.001;
 				camera.position.x = paddle2.position.x + 300;
-				camera.position.z = paddle2.position.z + 800;
 				camera.rotation.x = 0;
+				// camera.position.z = paddleDepth + 800;
 				camera.rotation.y = 40 * Math.PI/180;
 				camera.rotation.z = 90 * Math.PI/180;
 			}
@@ -226,9 +181,8 @@ export default defineComponent({
 				if (camera.position.x < (paddle2.position.x + 300))
 				{
 					camera.position.x += ((paddle2.position.x + 300) - (paddle1.position.x - 300)) * 0.01
-					var anglecam = (camera.position.x >= 0 ) ? 90 * Math.PI/180 : -90 * Math.PI/180
 					camera.position.y = Math.sqrt(Math.pow(paddle1.position.x - 300, 2) - Math.pow(camera.position.x, 2))
-					camera.rotation.z = anglecam + Math.atan(camera.position.y / camera.position.x)
+					camera.rotation.z = ((camera.position.x >= 0 ) ? 90 * Math.PI/180 : -90 * Math.PI/180) + Math.atan(camera.position.y / camera.position.x)
 					camera.rotation.y = -40 * Math.PI/180 * Math.sin(-camera.rotation.z)
 					camera.rotation.x = 40 * Math.PI/180 * Math.cos(-camera.rotation.z)
 				}
@@ -239,10 +193,9 @@ export default defineComponent({
 			{
 				if (camera.position.x > (paddle1.position.x - 300))
 				{
-					camera.position.x -= ((paddle2.position.x + 300) - (paddle1.position.x - 300)) * 0.01
-					var anglecam = (camera.position.x >= 0 ) ? 90 * Math.PI/180 : -90 * Math.PI/180
+					camera.position.x -= ((paddle2.position.x + 300) - (paddle1.position.x - 300)) * 0.01 
 					camera.position.y = -Math.sqrt(Math.pow(paddle1.position.x - 300, 2) - Math.pow(camera.position.x, 2))
-					camera.rotation.z = anglecam + Math.atan(camera.position.y / camera.position.x)
+					camera.rotation.z = ((camera.position.x >= 0 ) ? 90 * Math.PI/180 : -90 * Math.PI/180) + Math.atan(camera.position.y / camera.position.x)
 					camera.rotation.y = -40 * Math.PI/180 * Math.sin(-camera.rotation.z)
 					camera.rotation.x = 40 * Math.PI/180 * Math.cos(-camera.rotation.z)
 				}
@@ -252,7 +205,6 @@ export default defineComponent({
 		},
 		toggle(e: any) {
 			const target = <HTMLElement>document.getElementById('div3d')
-			console.log("toggle 1", this.$q.fullscreen.isActive)
 			this.$q.fullscreen.toggle(target)
 				.then(() => {
 				})
@@ -267,7 +219,6 @@ export default defineComponent({
 			timeOutFunctionId = setTimeout(this.onResize, 200);
 		},
 		onResize() {
-			console.log("onresize")
 			if (this.$q.fullscreen.isActive == true) {
 
 				if (window.screen.orientation.type == "portrait-primary") {
@@ -276,8 +227,8 @@ export default defineComponent({
 					this.canvas.width = window.innerWidth;
 					this.canvas_txt.height = window.innerWidth / 1.6;
 					this.canvas_txt.width = window.innerWidth;
-					var VIEW_ANGLE = 50, ASPECT = this.canvas_txt.width / this.canvas_txt.height, NEAR = 0.1, FAR = 5000;
-					camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+					camera = new THREE.PerspectiveCamera( VIEW_ANGLE, this.canvas_txt.width / this.canvas_txt.height, NEAR, FAR);
+					camera.position.z = paddleDepth + 800;
 					
 				}
 				else {
@@ -286,8 +237,8 @@ export default defineComponent({
 					this.canvas.height = window.innerHeight;
 					this.canvas_txt.width = window.innerWidth;
 					this.canvas_txt.height = window.innerHeight;
-					var VIEW_ANGLE = 50, ASPECT = this.canvas_txt.width / this.canvas_txt.height, NEAR = 0.1, FAR = 5000;
-					camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+					camera = new THREE.PerspectiveCamera( VIEW_ANGLE, this.canvas_txt.width / this.canvas_txt.height, NEAR, FAR);
+					camera.position.z = paddleDepth + 800;
 				}
 			}
 			else {
@@ -296,8 +247,8 @@ export default defineComponent({
 				this.canvas.width = window.innerWidth / 1.5;
 				this.canvas_txt.height = window.innerWidth / 1.5 / 1.6;
 				this.canvas_txt.width = window.innerWidth / 1.5;
-				var VIEW_ANGLE = 50, ASPECT = this.canvas_txt.width / this.canvas_txt.height, NEAR = 0.1, FAR = 5000;
-				camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+				camera = new THREE.PerspectiveCamera( VIEW_ANGLE, this.canvas_txt.width / this.canvas_txt.height, NEAR, FAR);
+				camera.position.z = paddleDepth + 800;
 			}
 			this.draw();
 		},
@@ -409,7 +360,3 @@ li {
 	display: inline-block;
 }
 </style>
-/*
-
-
-*/
