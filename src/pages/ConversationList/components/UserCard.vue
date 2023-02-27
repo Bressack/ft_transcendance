639e39459cde3a8075eaef5402619605a662086f
bressack <tharchen@student.42.fr>
2023-02-27 22:26:03 +0100

    "reformat UserCard.vue"

--- src/pages/ConversationList/components/UserCard.vue
+++ src/pages/ConversationList/components/UserCard.vue
@@ -1,68 +1,66 @@
 <template>
+  <q-item clickable v-ripple class="usermenu" manual-focus :focused="$store.active_channel === channelId">
+    <q-item-section style="max-width: 50px;" @click="goProfilPage">
+      <q-avatar class="avatar">
+        <img size="20px" :src="`/api/avatar/${username}/thumbnail`">
+        <div :class="getLoginStatus()" class="loginstatus" />
+      </q-avatar>
+    </q-item-section>
+    <q-item-section class="name" @click="goProfilPage">
+      {{ username }}
+    </q-item-section>
 
-    <q-item clickable v-ripple class="usermenu"
-					manual-focus
-					:focused="$store.active_channel === channelId">
-      <q-item-section style="max-width: 50px;" @click="goProfilPage">
-        <q-avatar class="avatar">
-          <img size="20px" :src="`/api/avatar/${username}/thumbnail`">
-          <div :class="getLoginStatus()" class="loginstatus"/>
-        </q-avatar>
-      </q-item-section>
-      <q-item-section class="name" @click="goProfilPage">
-        {{ username }}
-      </q-item-section>
+    <q-item-section side class="tata text-right">
+      <q-icon v-if="shortcut_profile" class="shortcut" name="person" color="cyan" @click="goProfilPage" />
+      <q-icon v-if="shortcut_block" class="shortcut" name="person_off" color="red" @click="block" />
+      <q-icon v-if="shortcut_unblock" class="shortcut" name="done" color="green" @click="block" />
+      <q-icon v-if="shortcut_play" class="shortcut" name="mdi-gamepad-variant-outline" color="green"
+        @click="goGameOptions" />
+      <q-icon v-if="shortcut_chat" class="shortcut" name="chat" color="orange" @click="goChat" />
+      <q-icon v-if="shortcut_unfollow" class="shortcut" name="cancel" color="red" @click="unfollow" />
+      <q-icon v-if="shortcut_follow" class="shortcut" name="done" color="green" @click="follow" />
+    </q-item-section>
 
-      <q-item-section side class="tata text-right">
-        <q-icon v-if="shortcut_profile"   class="shortcut" name="person"     color="cyan"   @click="goProfilPage" />
-        <q-icon v-if="shortcut_block"     class="shortcut" name="person_off" color="red"    @click="block" />
-        <q-icon v-if="shortcut_unblock"   class="shortcut" name="done"       color="green"  @click="block" />
-        <q-icon v-if="shortcut_play"      class="shortcut" name="mdi-gamepad-variant-outline" color="green"  @click="goGameOptions" />
-        <q-icon v-if="shortcut_chat"      class="shortcut" name="chat"       color="orange" @click="goChat" />
-        <q-icon v-if="shortcut_unfollow"  class="shortcut" name="cancel"     color="red"    @click="unfollow" />
-        <q-icon v-if="shortcut_follow"    class="shortcut" name="done"       color="green"  @click="follow" />
-      </q-item-section>
+    <q-item-section side class="toto">
+      <q-icon name="more_vert" color="#F7F7FF" class="toto">
+        <q-menu class="bg-grey-9 text-white" auto-close>
 
-      <q-item-section side class="toto">
-        <q-icon name="more_vert" color="#F7F7FF" class="toto">
-          <q-menu class="bg-grey-9 text-white" auto-close>
+          <q-list style="min-width: 100px">
 
-            <q-list style="min-width: 100px">
+            <q-item v-if="menu_profile" clickable @click="goProfilPage">
+              <q-item-section>Profile</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_profile" clickable @click="goProfilPage">
-                <q-item-section>Profile</q-item-section>
-              </q-item>
+            <q-item v-if="menu_play" clickable @click="goGameOptions">
+              <q-item-section>Invite to play</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_play" clickable @click="goGameOptions">
-                <q-item-section>Invite to play</q-item-section>
-              </q-item>
+            <q-item v-if="menu_chat" clickable :to="channelPath">
+              <q-item-section>Chat</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_chat" clickable :to="channelPath">
-                <q-item-section>Chat</q-item-section>
-              </q-item>
+            <q-item v-if="menu_unblock" clickable @click="block">
+              <q-item-section>Unblock</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_unblock" clickable @click="block">
-                <q-item-section>Unblock</q-item-section>
-              </q-item>
+            <q-item v-if="menu_follow" clickable @click="follow">
+              <q-item-section>Follow</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_follow" clickable @click="follow">
-                <q-item-section>Follow</q-item-section>
-              </q-item>
+            <q-separator dark />
 
-              <q-separator dark />
+            <q-item v-if="menu_block" clickable class="text-red-7" @click="block">
+              <q-item-section>Block</q-item-section>
+            </q-item>
 
-              <q-item v-if="menu_block" clickable class="text-red-7" @click="block">
-                <q-item-section>Block</q-item-section>
-              </q-item>
+            <q-item v-if="menu_unfollow" clickable class="text-red-7" @click="unfollow">
+              <q-item-section>Unfollow</q-item-section>
+            </q-item>
+          </q-list>
 
-              <q-item v-if="menu_unfollow" clickable class="text-red-7" @click="unfollow">
-                <q-item-section>Unfollow</q-item-section>
-              </q-item>
-            </q-list>
-
-          </q-menu>
-        </q-icon>
-      </q-item-section>
+        </q-menu>
+      </q-icon>
+    </q-item-section>
   </q-item>
 </template>
 
@@ -73,43 +71,43 @@ export default defineComponent({
   name: 'UserCard',
   components: {},
   props: {
-    username          : { type: String,  required: true },
-    icon_name         : { type: String,  default : ''   },
-    icon_color        : { type: String,  default : ''   },
+    username: { type: String, required: true },
+    icon_name: { type: String, default: '' },
+    icon_color: { type: String, default: '' },
 
-    menu_profile      : { type: Boolean, default: false },
-    menu_block        : { type: Boolean, default: false },
-    menu_unblock      : { type: Boolean, default: false },
-    menu_play         : { type: Boolean, default: false },
-    menu_chat         : { type: Boolean, default: false },
-    menu_follow       : { type: Boolean, default: false },
-    menu_unfollow     : { type: Boolean, default: false },
+    menu_profile: { type: Boolean, default: false },
+    menu_block: { type: Boolean, default: false },
+    menu_unblock: { type: Boolean, default: false },
+    menu_play: { type: Boolean, default: false },
+    menu_chat: { type: Boolean, default: false },
+    menu_follow: { type: Boolean, default: false },
+    menu_unfollow: { type: Boolean, default: false },
 
-    shortcut_profile  : { type: Boolean, default: false },
-    shortcut_block    : { type: Boolean, default: false },
-    shortcut_unblock  : { type: Boolean, default: false },
-    shortcut_play     : { type: Boolean, default: false },
-    shortcut_chat     : { type: Boolean, default: false },
-    shortcut_follow   : { type: Boolean, default: false },
-    shortcut_unfollow : { type: Boolean, default: false },
+    shortcut_profile: { type: Boolean, default: false },
+    shortcut_block: { type: Boolean, default: false },
+    shortcut_unblock: { type: Boolean, default: false },
+    shortcut_play: { type: Boolean, default: false },
+    shortcut_chat: { type: Boolean, default: false },
+    shortcut_follow: { type: Boolean, default: false },
+    shortcut_unfollow: { type: Boolean, default: false },
   },
   data() {
     return {
     }
   },
   computed: {
-	channelId() {
-		return this.$store.getChannelIDByUsername(this.username)
-	},
-	channelPath() {
-		return `/channel/${this.channelId}`
-	}
+    channelId() {
+      return this.$store.getChannelIDByUsername(this.username)
+    },
+    channelPath() {
+      return `/channel/${this.channelId}`
+    }
   },
   methods: {
     getLoginStatus() {
-	//TODO: check login status
-    //   if (this.$storeChat.connectedUsers.includes(this.username))
-    //     return 'ONLINE-status'
+      //TODO: check login status
+      //   if (this.$storeChat.connectedUsers.includes(this.username))
+      //     return 'ONLINE-status'
       return 'OFFLINE-status'
     },
     goProfilPage() {
